68765f101238285d60b181e5f1f54b9cda7f797f
Vinicius Leite <viniciusleite.desenvolvedor@gmail.com>
2021-02-14 16:10:09 -0300

    deploy initial

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/calculatorMyCafe`));
 
-app.get('/*',(req,res) => {
+app.get('/',(req,res) => {
   res.sendFile(path.join(`${__dirname}/dist/calculatorMyCafe/index.html`));
 });
 
