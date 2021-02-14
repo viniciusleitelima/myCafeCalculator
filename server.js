86e7d5e76b0ad6688ec199b53f0708cafb6dc96f
Vinicius Leite <viniciusleite.desenvolvedor@gmail.com>
2021-02-14 16:04:51 -0300

    deploy initial

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.nmp_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/calculatorMyCafe`));
 
 app.get('/*',(req,res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/calculatorMyCafe/index.html`));
 });
 
-app.listen(4200);
+app.listen();
