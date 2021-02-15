1dd3980c400067ca69fdee636685a5b1a81b4cf8
Vinicius Leite <viniciusleite.desenvolvedor@gmail.com>
2021-02-15 17:23:21 -0300

    inclusao calculo rubis

--- src/app/components/distribuicao/distribuicao.component.ts
+++ src/app/components/distribuicao/distribuicao.component.ts
@@ -10,107 +10,131 @@ export class DistribuicaoComponent implements OnInit {
     total:null
   }
 
+  rubis = {
+    total:null
+  }
+
 
   cidadaos = [
     {
       id:1,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:2,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:3,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:4,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:5,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:6,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:7,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:8,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:9,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:10,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:11,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:12,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:13,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:14,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:15,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:16,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:17,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:18,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:19,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
     {
       id:20,
       trofeus:null,
-      valor_premio:null
+      valor_premio:null,
+      valor_rubis:null
     },
   ];
 
@@ -130,8 +154,10 @@ export class DistribuicaoComponent implements OnInit {
     for(let i=0; i < this.cidadaos.length;i++){
       if(this.cidadaos[i].trofeus){
         this.cidadaos[i].valor_premio = Math.round((this.cidadaos[i].trofeus/this.trofeus.total) * this.diamantes.total).toString();
+        this.cidadaos[i].valor_rubis = Math.round((this.cidadaos[i].trofeus/this.trofeus.total) * this.rubis.total).toString();
       }else{
         this.cidadaos[i].valor_premio = null;
+        this.cidadaos[i].valor_rubis = null;
       }
     }
   }
